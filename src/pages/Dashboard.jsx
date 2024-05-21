c4b42a69646b40b89b9002022f3783446ace75eb
leBryanXD <111110243+leBryanXD@users.noreply.github.com>
2024-05-20 22:37:18 -0600

    dashboard maquetado

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,18 +1,19 @@
-import { Col, Container, Row } from "react-bootstrap";
+import { Col, Container, Row, Table } from "react-bootstrap";
 import imgDashborad from "../images/imgdashboard.png";
 import { Link } from "react-router-dom";
 
 const Dashboard = () => {
+  const theadContent = ["ID", "Servicio", "Fecha", "Estado"];
   return (
     <>
       <Container>
         <Row>
-          <Col>
+          <Col sm={12} md={6} className="text-center">
             <img src={imgDashborad} alt="imagen principal" />
           </Col>
-          <Col>
+          <Col sm={12} md={6}>
             {" "}
-            <h1>Soporte Técnico</h1>
+            <h1 className="mt-5 mb-5 text-center">Soporte Técnico</h1>
             <p>
               Bienvenido a nuestra empresa de soporte técnico de IT, donde nos
               comprometemos a ser tu aliado confiable en el complejo mundo de la
@@ -20,12 +21,36 @@ const Dashboard = () => {
               desafíos informáticos con soluciones rápidas y efectivas.
             </p>
             <div>
-              <Link to={"/"}>Crear nuevo ticket other </Link>
+              <Link
+                to={"/"}
+                style={{
+                  textDecoration: "none",
+                  color: "#656C95",
+                  fontSize: "1.3rem",
+                }}
+              >
+                Crear nuevo ticket{" "}
+              </Link>
             </div>
           </Col>
         </Row>
         <Row>
-          <Col>1 of 3</Col>
+          <Col>
+            {" "}
+            <h2 className="text-center mt-5 mb-4">Mis Tickets</h2>
+            <Table striped bordered hover className="text-center">
+              <thead>
+                <tr className="">
+                  {theadContent.map((content) => (
+                    <th key={content} style={{ backgroundColor: "#99ACFF" }}>
+                      {content}
+                    </th>
+                  ))}
+                </tr>
+              </thead>
+              <tbody></tbody>
+            </Table>
+          </Col>
         </Row>
       </Container>
       <section className=""></section>
